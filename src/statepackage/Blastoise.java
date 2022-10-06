2c3d5b4b2613fceff099f87c732d1c3905d1287e
JaakkoRitvas <47250409+JaakkoRitvas@users.noreply.github.com>
2022-10-06 06:01:15 +0300

    Add files via upload

---
+++ src/statepackage/Blastoise.java
@@ -0,0 +1,45 @@
+package statepackage;
+
+public class Blastoise extends State {
+
+	Blastoise(Pokemon pokemon) {
+		super(pokemon);
+		pokemon.setNimi("Blastoise");
+		this.toiminta();
+	}
+	
+	public void blastoisenTemppu() {
+		System.out.println("Ammun tykeistä vettä");
+	}
+
+	@Override
+	public void kehity() {
+	}
+
+	@Override
+	public void toiminta() {
+		System.out.println(pokemon.getNimi() + " viimeinen evoluutio!");
+		System.out.println("Nyt sit vaan seikkailua ympäriinsä");
+
+		while (true) {
+
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			
+			pokemon.nostaTasoa();
+			System.out.println("Kehityin tason ylöspäin olen nyt " + pokemon.getTaso());
+			
+			if(pokemon.getTaso() == 100) {
+				System.out.println("Viimeinen leveli, en enää kehity");
+				break;
+			}
+
+		}
+
+	}
+
+}
---
+++ src/statepackage/State.java
@@ -0,0 +1,17 @@
+package statepackage;
+
+public abstract class State {
+
+	Pokemon pokemon;
+	
+	State(Pokemon pokemon){
+		this.pokemon = pokemon;
+	}
+	
+	//State vaihtuu
+	public abstract void kehity();
+	
+	//Toimintoja mitä tekee
+	public abstract void toiminta();
+	
+}
